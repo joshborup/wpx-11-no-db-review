f8fd446b4d3216400c64b3440bf1e1667a823a95
Josh Borup <joshborup@Joshs-MacBook-Pro.local>
2019-03-28 12:38:02 -0700

    finished review

--- src/components/UserFavorites/UserFavorites.js
+++ src/components/UserFavorites/UserFavorites.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "./UserFavorites.css";
+const posterBasePath = "http://image.tmdb.org/t/p/w185/";
 
-export default function UserFavorites() {
-  return <div>This is the User Favorites</div>;
+export default function UserFavorites(props) {
+  let mappedFavorites = props.favorites.map(movie => {
+    return (
+      <div key={movie.id}>
+        <h1>{movie.title}</h1>
+        <h2>{movie.popularity}</h2>
+        <img src={posterBasePath + movie.poster_path} alt="" />
+        <span>{movie.overview}</span>
+      </div>
+    );
+  });
+  return <div className="favorites-container">{mappedFavorites}</div>;
 }
